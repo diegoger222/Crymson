870a058bb7cc96fc16ea1621a9f5e2b46271e860
diegoger222 <runyxp@gmail.com>
2022-03-24 22:00:14 +0100

    enlace escena

--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPrincipal : MonoBehaviour
 {
+
+    
     public void IniciarJuego() {
         //abrir juego
         Debug.Log("Se inicia el juego.");
+        SceneManager.LoadScene("Game");
     }
 
     public void SalirJuego() {
