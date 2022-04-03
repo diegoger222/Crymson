902518707b3bb23ddc65cd7fa22bb9990d3bfd97
diegoger222 <runyxp@gmail.com>
2022-04-03 21:14:24 +0200

    e

--- Assets/Scripts/Habilidades/HudHabilidades.cs
+++ Assets/Scripts/Habilidades/HudHabilidades.cs
@@ -23,6 +23,7 @@ public class HudHabilidades : MonoBehaviour
     {
         habilidadesHUD = GameObject.FindGameObjectWithTag("HudHabilidades"); //Obtenes el game object donde estan los marcos de habilidades
         Iniciar();
+        ListaHabi[0].SlotIU.transform.GetChild(1).GetComponent<Image>().enabled = false;
     }
 
 
@@ -31,7 +32,7 @@ public class HudHabilidades : MonoBehaviour
     {
        
         if (Input.GetKeyDown("l")){
-            UsoHabilidad(0);
+            UsoHabilidad(0,50);
         }
     }
     public void Iniciar()
@@ -58,12 +59,54 @@ public class HudHabilidades : MonoBehaviour
 
 
 
-    public void UsoHabilidad(int nh)
+    public void UsoHabilidad(int nh,float cooldown)
     {
         ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>().text = "10"; //transform.GetChild(3).GetComponent<Text>() = 10;
-
+        bool isCooldown = true;
+        Image auxIma = ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>();
+        Text  auxText = ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>();
+        auxIma.enabled = true;
+        while (isCooldown)
+        {
+            float auxFloat  = 1 / cooldown * Time.deltaTime;
+            auxIma.fillAmount -= auxFloat;
+            auxText.text = ((int)(cooldown*auxFloat)).ToString();
+            if (auxIma.fillAmount <= 0) {
+                auxIma.fillAmount = 1;
+                auxIma.enabled = false;
+                isCooldown = false;
+            }
+        }
 
 
     }
+
+    /*
+     public void UsarStamina(float cantidad)
+    {
+        if(currentStamina- cantidad >= 0)
+        {
+            currentStamina -= cantidad;
+
+            if(regen != null) {
+                StopCoroutine(regen);
+            }
+
+            regen = StartCoroutine(RegenStamina());
+        }
+    }
+
+
+    private IEnumerator RegenStamina()
+    {
+        yield return new WaitForSeconds(2f);
+
+        while (currentStamina < maxStamina)
+        {
+            currentStamina += maxStamina / 100;
+            yield return regenTick;
+        }
+    }
+    */
 
 }
