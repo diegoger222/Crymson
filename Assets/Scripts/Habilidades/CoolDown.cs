d6c1feaa1c987a1d1bea54b46b728b9114e56f2a
diegoger222 <runyxp@gmail.com>
2022-04-04 11:52:57 +0200

    Cooldown habilidades

--- Assets/Scripts/Habilidades/CoolDown.cs
+++ Assets/Scripts/Habilidades/CoolDown.cs
@@ -9,34 +9,66 @@ public class CoolDown : MonoBehaviour
     public float coolDown;
     private float coolDownFill; //valor entre 0 y 1 de la imagen
     private float coolDownText;
-    private bool iscoolDown;
+    private bool iscoolDown = false;
     private Image fill;
     private Text textN;
 
     public List<GameObject> habilidades;
-    private List<int> coolDowns = new List<int>();
+    public List<float> coolDownVar = new List<float>();
+    private List<float> coolDowns = new List<float>();
+    private List<bool> isCoolDowns = new List<bool>();
+    private List<Image> coolDownfills = new List<Image>();
+    private List<Text> coolDowntexts = new List<Text>();
+    public List<HudHabilidades.HabilU> ListaHabi = new List<HudHabilidades.HabilU>();
+    //private List<GameObject> habilidadesIU = new List<GameObject>();
     
     // Start is called before the first frame update
     void Start()
     {
+        /*
         for(int i =0;i<habilidades.Count; i++)
         {
             coolDowns.Add(habilidades[i].GetComponent<HabilidadMovimiento>().CoolDownTime());
+            isCoolDowns.Add(false);
         }
         coolDownText = coolDowns[0];
-        coolDown = coolDowns[0];
-        iscoolDown = false;
+       // coolDown = coolDowns[0];
+        //iscoolDown = false;
         coolDownFill = 1;
+        */
     }
 
     // Update is called once per frame
    void Update()
     {
-        if (iscoolDown)
-        {
-            Debug.Log("hola");
+
+      //  if (iscoolDown)
+        //{
+          //  Debug.Log("hola");
           //  Debug.Log(fill.fillAmount);
           //  coolDownFill ;
+          for(int z = 0;z< coolDowns.Count; z++)
+            {
+            Debug.Log(z);
+                if (isCoolDowns[z])
+                {
+                
+                    coolDownVar[z] -= Time.deltaTime;
+                    coolDownfills[z].fillAmount -= 1 / coolDowns[z] * Time.deltaTime;
+                    coolDowntexts[z].text = ((int)coolDownVar[z]).ToString();
+                    if(coolDownfills[z].fillAmount <= 0.0f)
+                    {
+                        coolDownfills[z].enabled = false;
+                        coolDowntexts[z].enabled= false;
+                       ResetCoolDown(z);
+                    }
+                    // coolDowns[z] -= Time.deltaTime;
+
+
+                }
+
+            }
+          /*
             coolDownText -= Time.deltaTime;
             fill.fillAmount  -= 1 / coolDown * Time.deltaTime;
             textN.text = ((int)coolDownText).ToString();
@@ -46,7 +78,8 @@ public class CoolDown : MonoBehaviour
                 textN.enabled = false;
                 ResetCoolDown();
             }
-        }
+          */
+        //}
     }
 
     public float  GetValorFill()
@@ -74,10 +107,31 @@ public class CoolDown : MonoBehaviour
        
     }
 
-    public void ResetCoolDown()
+    public void ResetCoolDown(int z)
     {
-        iscoolDown = false;
-        coolDownFill = 1;
-        coolDownText = coolDown;
+        isCoolDowns[z] = false;
+        //iscoolDown = false;
+        coolDownfills[z].fillAmount = 1;
+        // coolDownFill = 1;
+        coolDownVar[z] = coolDowns[z];
+       // coolDownText = coolDown;
+    }
+    public void SetHabilidadesIU(List<HudHabilidades.HabilU> ha)
+    {
+        ListaHabi = ha;
+        for (int i = 0; i < ha.Count; i++)
+        {
+            coolDowns.Add(ha[i].habilidadIU.GetComponent<HabilidadMovimiento>().CoolDownTime());
+            coolDownfills.Add(ha[i].SlotIU.transform.GetChild(1).GetComponent<Image>());
+            coolDowntexts.Add(ha[i].SlotIU.transform.GetChild(3).GetComponent<Text>());
+            coolDownVar.Add(coolDowns[i]);
+           // coolDownFills
+            isCoolDowns.Add(false);
+        }
+    }
+
+    public void UsoHabilidad(int a)
+    {
+        isCoolDowns[a] = true;
     }
 }
--- Assets/Scripts/Habilidades/HudHabilidades.cs
+++ Assets/Scripts/Habilidades/HudHabilidades.cs
@@ -8,7 +8,8 @@ public class HudHabilidades : MonoBehaviour
     public Image imagenNormal;
     private GameObject habilidadesHUD;
     public bool isCooldown = true;
-    public GameObject habilidad1;
+    public GameObject auxCold;
+    public List<GameObject> Habilidades = new List<GameObject>();
     public class HabilU
     {
         public GameObject SlotIU { get; set; }
@@ -23,7 +24,14 @@ public class HudHabilidades : MonoBehaviour
     {
         habilidadesHUD = GameObject.FindGameObjectWithTag("HudHabilidades"); //Obtenes el game object donde estan los marcos de habilidades
         Iniciar();
-        ListaHabi[0].SlotIU.transform.GetChild(3).GetComponent<Text>().enabled = false;
+        for(int i = 0; i < ListaHabi.Count; i++)
+        {
+            ListaHabi[i].SlotIU.transform.GetChild(3).GetComponent<Text>().enabled = false;
+            ListaHabi[i].SlotIU.transform.GetChild(1).GetComponent<Image>().enabled = false;
+        }
+      
+        auxCold.GetComponent<CoolDown>().SetHabilidadesIU(ListaHabi);
+        
     }
 
 
@@ -32,7 +40,11 @@ public class HudHabilidades : MonoBehaviour
     {
        
         if (Input.GetKeyDown("l")){
-            UsoHabilidad(0,2);
+            UsoHabilidad(0);
+        }
+        if (Input.GetKeyDown("k"))
+        {
+            UsoHabilidad(1);
         }
     }
     public void Iniciar()
@@ -43,7 +55,7 @@ public class HudHabilidades : MonoBehaviour
             GameObject habi = habilidadesHUD.transform.GetChild(a).gameObject; // obtenemos la habilidad correspondiente al index actual (a) 
       // Debug.Log(habi);
 
-          HabilU aux = new HabilU() { SlotIU = habi,habilidadIU =habilidad1 };
+          HabilU aux = new HabilU() { SlotIU = habi,habilidadIU =Habilidades[a] };
             ListaHabi.Add(aux);
             
 
@@ -59,45 +71,47 @@ public class HudHabilidades : MonoBehaviour
 
 
 
-    public void UsoHabilidad(int nh,float cooldown)
+    public void UsoHabilidad(int nh)
     {
+        
         
         ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>().enabled = true;
         ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>().enabled = true;
+        auxCold.GetComponent<CoolDown>().UsoHabilidad(nh);
        // ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>().text = "10"; //transform.GetChild(3).GetComponent<Text>() = 10;
 
-        ListaHabi[nh].habilidadIU.GetComponent<CoolDown>().StartTime(ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>(), ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>());
-       // float auxFloat;
+        //  ListaHabi[nh].habilidadIU.GetComponent<CoolDown>().StartTime(ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>(), ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>());
+        // float auxFloat;
         //Image auxIma = ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>();
         //Text  auxText = ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>();
         //auxIma.enabled = true;
         //auxFloat = 1 / cooldown * Time.deltaTime;
 
-        if (ListaHabi[nh].habilidadIU.GetComponent<CoolDown>().ISCoolDown())
-        {
+        // if (ListaHabi[nh].habilidadIU.GetComponent<CoolDown>().ISCoolDown())
+        //{
 
-            float aux = ListaHabi[nh].habilidadIU.GetComponent<CoolDown>().GetValorFill();
-            Debug.Log(aux);
-           // ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>().fillAmount = aux;
-           // ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>().text = ((int)(ListaHabi[nh].habilidadIU.GetComponent<CoolDown>().GetValorText())).ToString();
-          //  float a = ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>().fillAmount;
-           // Debug.Log(a);
-            //poner una variable con Ienumerator para el coldown
-            /*
-            if (a<=0.0f)
-            {
-                ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>().fillAmount = 1;
-                ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>().enabled = false;
-               // ListaHabi[nh].habilidadIU.GetComponent<CoolDown>().ResetCoolDown();
-                isCooldown = false;
-            }
-            */
-            /*
-            if (ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>().fillAmount <= 0.0f) {
-                ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>().fillAmount = 1;
-                ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>().enabled = false;
-                isCooldown = false;
-            }*/
+        //   float aux = ListaHabi[nh].habilidadIU.GetComponent<CoolDown>().GetValorFill();
+        //  Debug.Log(aux);
+        // ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>().fillAmount = aux;
+        // ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>().text = ((int)(ListaHabi[nh].habilidadIU.GetComponent<CoolDown>().GetValorText())).ToString();
+        //  float a = ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>().fillAmount;
+        // Debug.Log(a);
+        //poner una variable con Ienumerator para el coldown
+        /*
+        if (a<=0.0f)
+        {
+            ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>().fillAmount = 1;
+            ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>().enabled = false;
+           // ListaHabi[nh].habilidadIU.GetComponent<CoolDown>().ResetCoolDown();
+            isCooldown = false;
+        }
+        */
+        /*
+        if (ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>().fillAmount <= 0.0f) {
+            ListaHabi[nh].SlotIU.transform.GetChild(1).GetComponent<Image>().fillAmount = 1;
+            ListaHabi[nh].SlotIU.transform.GetChild(3).GetComponent<Text>().enabled = false;
+            isCooldown = false;
+        }*/
         }
         
 
@@ -133,4 +147,4 @@ public class HudHabilidades : MonoBehaviour
 
 
 
-}
+
