71a947510115dfd1eb18c4c4f668192f326a5664
H4T3C0MPU73R5 <adrianp1101@gmail.com>
2022-04-22 17:35:29 +0200

    Detección de mando
    Hay que mirar cómo enlazar el código del botón con el botón creado en la interfaz.

--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -2,11 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
 
 public class MenuPrincipal : MonoBehaviour
 {
+    private bool hayMando = false;
+    [SerializeField] private GameObject botonIniciar; //hay que enlazar el código con el botón de iniciar juego
 
-    
+    void Start() {
+        Debug.Log("Comprueba si hay mandos");
+        string[] listaMandos = Input.GetJoystickNames();
+        string mandos = "";
+        for (int i = listaMandos.Length - 1; i >= 0; i--) {
+            mandos += listaMandos[i];
+            if (mandos.Length >= 10) {
+                hayMando = true;
+                Debug.Log("Hay mando");
+                break;
+            }
+        }
+        if (hayMando) { //esto destaca el primer botón. Sólo hay que enlazar el código con el de la interfaz
+            var eventSystem = EventSystem.current;
+            eventSystem.SetSelectedGameObject(botonIniciar, new BaseEventData(eventSystem));
+        } else Debug.Log("No hay mando");
+    }
+
+
+
     public void IniciarJuego() {
         //abrir juego
         Debug.Log("Se inicia el juego.");
