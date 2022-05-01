04383a111cbe14a338c813b01a97500752e20b54
H4T3C0MPU73R5 <adrianp1101@gmail.com>
2022-05-01 19:06:36 +0200

    Falta de ortografía corregida

--- Assets/Scripts/Personaje/Experiencia.cs
+++ Assets/Scripts/Personaje/Experiencia.cs
@@ -17,7 +17,7 @@ public class Experiencia : MonoBehaviour
     private float exp_nivel; // lo que te cuesta subir de nivel base
     private int var_nivel; // experiencia extra que cuesta por cada nivel 
     private float exp_guar; // experiencia guardada
-    private float exp_acomul; //experiencia acomulada
+    private float exp_acumul; //experiencia acomulada
     private int puntos_skills;
     private float exp_perdi; //experiencia perdida al morir
     private GameObject skillpoints;
@@ -29,26 +29,26 @@ public class Experiencia : MonoBehaviour
         exp_nivel = 125; 
         var_nivel = 35;
         exp_guar = 0;
-        exp_acomul = 120;
+        exp_acumul = 120;
         puntos_skills = 0; 
         experienciaNecesaria = 125;
-        textoValorBarraExperiencia.text = "" + exp_acomul.ToString() + " / " + experienciaNecesaria.ToString() + " XP";
+        textoValorBarraExperiencia.text = "" + exp_acumul.ToString() + " / " + experienciaNecesaria.ToString() + " XP";
     }
 
     // Update is called once per frame
     void Update()
     {
-        text_exp.text = exp_acomul.ToString();
+        text_exp.text = exp_acumul.ToString();
         text_nivel.text = act_nivel.ToString();
 
-        barra_exp.fillAmount = exp_acomul / experienciaNecesaria;
+        barra_exp.fillAmount = exp_acumul / experienciaNecesaria;
         skillpoints = GameObject.FindGameObjectWithTag("Statsm");
     }
 
     public void ExperienciaPermanente()
     {
        
-        exp_guar = exp_acomul;
+        exp_guar = exp_acumul;
         
         experienciaNecesaria = exp_nivel + var_nivel * (act_nivel - 1); //experiencia de nivel necesaria;
         while(exp_guar> experienciaNecesaria)
@@ -59,20 +59,20 @@ public class Experiencia : MonoBehaviour
             skillpoints.GetComponent<PuntosEstadisticas>().SubirPuntos();
             skillpoints.GetComponent<PuntosEstadisticas>().Nivel(act_nivel);
 
-            exp_acomul = aux_exp;
+            exp_acumul = aux_exp;
             exp_guar = 0;
-            barra_exp.fillAmount = exp_acomul / experienciaNecesaria;
-            textoValorBarraExperiencia.text = "" + exp_acomul.ToString() + " / " + experienciaNecesaria.ToString() + " XP";
+            barra_exp.fillAmount = exp_acumul / experienciaNecesaria;
+            textoValorBarraExperiencia.text = "" + exp_acumul.ToString() + " / " + experienciaNecesaria.ToString() + " XP";
 
         }
     }
     
     public void GanarExperiencia(int cantidad)
     {
-        exp_acomul += cantidad;
+        exp_acumul += cantidad;
         //actualizamos la barra de experiencia y el texto con la cantidad de dicha
-        barra_exp.fillAmount = exp_acomul / experienciaNecesaria;
-        textoValorBarraExperiencia.text = "" + exp_acomul.ToString() + " / " + experienciaNecesaria.ToString() + " XP";
+        barra_exp.fillAmount = exp_acumul / experienciaNecesaria;
+        textoValorBarraExperiencia.text = "" + exp_acumul.ToString() + " / " + experienciaNecesaria.ToString() + " XP";
         
     }
     
